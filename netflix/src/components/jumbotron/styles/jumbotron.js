a029203dfd4308ad81fb0b41944629536ff80b57
sherylteoyt <sherylteoyt@users.noreply.github.com>
2020-12-07 11:26:46 +0800

    setup dependencies, cleaned up files, add in jumbotron.js, faqs.json and jumbo.json

---
+++ netflix/src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/macro'
+//macro is going to give us the name of the component in the dev tools 
+//works without any customisation would be 56924x... but macro gives us the component name
+
+export const Inner = styled.div`
+    display: flex;
+`;
