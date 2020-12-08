f6b3c16b82fee5182fb8e8269fb3a19e06f72ff4
sherylteoyt <sherylteoyt@users.noreply.github.com>
2020-12-08 19:40:37 +0800

    completed faq and accordion body

--- netflix/src/components/accordion/index.js
+++ netflix/src/components/accordion/index.js
@@ -30,12 +30,18 @@ Accordion.Item = function AccordionItem({ children, ...restProps }) {
 // In header, we want to get toggleShow and setToggleShow, and we have wrapped the Provider
 // Within Item, when we call it, we will be passing the header in as a child
 // Wrapped in Provider - to do that, we have to create a const for it, set to useContext
+// To dump out state, use this - <pre>{JSON.stringify(toggleShow, null, 2)}</pre>
 Accordion.Header = function AccordionHeader({ children, ...restProps }) {
   const { toggleShow, setToggleShow } = useContext(ToggleContext);
 
   return (
     <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
       {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="Open" />
+      )}
     </Header>
   );
 };
