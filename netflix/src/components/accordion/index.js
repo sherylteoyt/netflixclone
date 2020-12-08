e9219cd215ed324dfe9ca7948433765f9c3e743f
sherylteoyt <sherylteoyt@users.noreply.github.com>
2020-12-08 18:05:40 +0800

    completed accordion component, cleaned up eslint

---
+++ netflix/src/components/accordion/index.js
@@ -0,0 +1,47 @@
+import React, { createContext, useState, useContext } from 'react';
+import { Container, Frame, Title, Item, Inner, Header, Body } from './styles/accordion';
+
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useState(false);
+  return (
+    <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>
+    </ToggleContext.Provider>
+  );
+};
+
+// In header, we want to get toggleShow and setToggleShow, and we have wrapped the Provider
+// Within Item, when we call it, we will be passing the header in as a child
+// Wrapped in Provider - to do that, we have to create a const for it, set to useContext
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+
+  return (
+    <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+      {children}
+    </Header>
+  );
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext);
+
+  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+};
---
+++ netflix/src/components/accordion/styles/accordion.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div``;
+
+export const Item = styled.div``;
+
+export const Header = styled.div``;
+
+export const Body = styled.div``;
+
+export const Frame = styled.div``;
+
+// If have container which is a particular size, inner will be the content inside which the width is smaller than the container
+export const Inner = styled.div``;
+
+export const Title = styled.h1``;
