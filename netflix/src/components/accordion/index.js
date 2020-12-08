10c9a03587677c99e13d81d8630fed3da5b257d0
sherylteoyt <sherylteoyt@users.noreply.github.com>
2020-12-08 22:57:06 +0800

    Completed opt-form

--- netflix/src/components/accordion/index.js
+++ netflix/src/components/accordion/index.js
@@ -14,10 +14,6 @@ Accordion.Title = function AccordionTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
-Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
-  return <Frame {...restProps}>{children}</Frame>;
-};
-
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
   const [toggleShow, setToggleShow] = useState(false);
   return (
