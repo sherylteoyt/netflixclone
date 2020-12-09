e6661ca7a2b90e07c689c3172ebc853561d3acf2
Sheryl Teo <sheryl.teo@ntuclearninghub.com>
2020-12-09 15:13:25 +0800

    completed router

--- netflix/src/app.js
+++ netflix/src/app.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { FaqsContainer } from './containers/faqs';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Browse, Signin, Signup } from './pages';
+
+import * as ROUTES from './constants/routes';
 
 // Jumbotron.Pane - to split content into panels. Direction will tell us when the info will be flipped (right panel to left panel)
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
