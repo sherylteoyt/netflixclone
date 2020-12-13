b49c425b69882a9713f762ccf10832a481b072ed
Sheryl Teo <sheryl.teo@ntuclearninghub.com>
2020-12-13 19:13:27 +0800

    completed header profile

--- netflix/src/components/header/index.js
+++ netflix/src/components/header/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-
-import { Background, ButtonLink, Container, Logo } from './styles/header';
+import { Background, ButtonLink, Container, Dropdown, Feature, FeatureCallOut, Group, Logo, Link, Picture, Profile, Text } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
+
+Header.Feature = function HeaderFeature({ children, ...restProps}) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
@@ -21,4 +24,32 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
       <Logo {...restProps} />
     </ReactRouterLink>
   );
+};
+
+Header.Text = function HeaderText({ children, ...restProps}) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Picture = function HeaderPicture({ children, ...restProps }) {
+  return <Picture {...restProps}>{children}</Picture>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps}) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
 };
