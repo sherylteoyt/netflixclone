73cbcdeae5974692377e8c4c7bafb65d8149f647
Sheryl Teo <sheryl.teo@ntuclearninghub.com>
2020-12-14 18:10:24 +0800

    minor arrangement for feature component

--- netflix/src/components/feature/index.js
+++ netflix/src/components/feature/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Title, SubTitle } from './styles/feature';
 
+export default function Feature ({ children, ...restProps }) {
+    return<Container { ...restProps}>{children}</Container>;
+}
 
 Feature.Title = function FeatureTitle({ children, ...restProps }) {
     return <Title { ...restProps}>{children}</Title>;
@@ -9,7 +12,3 @@ Feature.Title = function FeatureTitle({ children, ...restProps }) {
 Feature.SubTitle = function FeatureSubTitle({ children, ...restProps }) {
     return <SubTitle {...restProps}>{children}</SubTitle>;
 };
-
-export default function Feature ({ children, ...restProps }) {
-    return<Container { ...restProps}>{children}</Container>;
-}
