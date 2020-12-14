6dde93c8365eaae6f32d5e6dcf7384e59df71283
Sheryl Teo <sheryl.teo@ntuclearninghub.com>
2020-12-14 17:58:45 +0800

    cleaned up accordion component

--- netflix/src/components/accordion/index.js
+++ netflix/src/components/accordion/index.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useContext } from 'react';
-import { Container, Title, Item, Inner, Header, Body } from './styles/accordion';
+import React, { useState, useContext, createContext } from 'react';
+import { Container, Frame, Title, Item, Inner, Header, Body } from './styles/accordion';
 
 const ToggleContext = createContext();
 
@@ -10,8 +10,13 @@ export default function Accordion({ children, ...restProps }) {
     </Container>
   );
 }
+
 Accordion.Title = function AccordionTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
 };
 
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
@@ -31,7 +36,7 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
   const { toggleShow, setToggleShow } = useContext(ToggleContext);
 
   return (
-    <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+    <Header onClick={() => setToggleShow(!toggleShow)} {...restProps}>
       {children}
       {toggleShow ? (
         <img src="/images/icons/close-slim.png" alt="Close" />
@@ -45,5 +50,11 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
   const { toggleShow } = useContext(ToggleContext);
 
-  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+  /* return toggleShow ? <Body {...restProps}>{children}</Body> : null; */
+
+  return (
+    <Body className={toggleShow ? 'open' : 'closed'} {...restProps}>
+      <span>{children}</span>
+    </Body>
+  );
 };
--- netflix/src/components/accordion/styles/accordion.js
+++ netflix/src/components/accordion/styles/accordion.js
@@ -5,6 +5,10 @@ export const Container = styled.div`
   border-bottom: 8px solid #222;
 `;
 
+export const Frame = styled.div`
+  margin-bottom: 40px;
+`;
+
 // If have container which is a particular size, inner will be the content inside which the width is smaller than the container
 export const Inner = styled.div`
   display: flex;
@@ -14,14 +18,31 @@ export const Inner = styled.div`
   margin: auto;
 `;
 
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-top: 0;
+  margin-bottom: 8px;
+  color: white;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+`;
+
 export const Item = styled.div`
   color: white;
+  margin: auto;
   margin-bottom: 10px;
   max-width: 700px;
   width: 100%;
 
   &:first-of-type {
     margin-top: 3em;
+  }
+  &:last-of-type {
+    margin-bottom: 0;
   }
 `;
 
@@ -40,9 +61,10 @@ export const Header = styled.div`
   img {
       filter: brightness(0) invert(1)
       width: 24px;
+      user-select: none;
 
       @media (max-width: 600px) {
-          width: 16px;0
+          width: 16px;
       }
   }
 
@@ -52,30 +74,28 @@ export const Header = styled.div`
 `;
 
 export const Body = styled.div`
-  max-height: 1200px;
-  transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
   font-size: 26px;
   font-weight: normal;
   line-height: normal;
   background: #303030;
-  padding: 0.8em 2.2em 0.8em 1.2em;
   white-space: pre-wrap;
   user-select: none;
-
+  overflow: hidden;
+  &.closed {
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
+  }
+  &.open {
+    max-height: 1200px;
+    transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
+  }
+  span {
+    display: block;
+    padding: 0.8em 2.2em 0.8em 1.2em;
+  }
   @media (max-width: 600px) {
     font-size: 16px;
     line-height: 22px;
-  }
-`;
-
-export const Title = styled.h1`
-  font-size: 50px;
-  line-height: 1.1;
-  margin-top: 0;
-  color: white;
-  text-align: center;
-
-  @media (max-width: 600px) {
-    font-size: 35px;
   }
 `;
