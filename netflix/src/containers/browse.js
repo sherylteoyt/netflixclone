bb554998083a6ab075063463276afd58b47487ef
Sheryl Teo <sheryl.teo@ntuclearninghub.com>
2020-12-14 16:29:39 +0800

    completed browse page with styling

--- netflix/src/containers/browse.js
+++ netflix/src/containers/browse.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
+import { FooterContainer } from './footer';
 import { FirebaseContext } from '../context/firebase';
 import { Card, Header, Loading } from '../components';
 import * as ROUTES from '../constants/routes';
@@ -75,9 +76,27 @@ export function BrowseContainer({ slides }) {
                 {slideRows.map((slideItem) => (
                     <Card key={`${category}-$(slideItem.title.toLowerCase()}`}>
                         <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        {/* <Card.Feature category={category}>
+                            <Player>
+                                <Player.Button />
+                                <Player.Video src="/videos/bunny.mp4" />
+                            </Player>
+                        </Card.Feature> */}
                     </Card>
                 ))}
             </Card.Group>
+            <FooterContainer />
         </>
     ) : (
             <SelectProfileContainer user={user} setProfile={setProfile} />
