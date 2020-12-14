2b47c476f91e82b1d6e269c9bf3500b73b79df20
Sheryl Teo <sheryl.teo@ntuclearninghub.com>
2020-12-14 18:07:51 +0800

    minor formatting for card styles

--- netflix/src/components/card/styles/card.js
+++ netflix/src/components/card/styles/card.js
@@ -14,11 +14,13 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 50px;
+
   > ${Title} {
     @media (max-width: 1000px) {
       margin-left: 30px;
     }
   }
+
   &:last-of-type {
     margin-bottom: 0;
   }
@@ -29,6 +31,7 @@ export const Group = styled.div`
   flex-direction: ${({ flexDirection }) => (flexDirection === 'row' ? 'row' : 'column')};
   ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
   ${({ margin }) => margin && `margin: ${margin}`};
+  
   > ${Container}:first-of-type {
     @media (min-width: 1100px) {
       margin-top: -100px;
@@ -86,24 +89,30 @@ export const Item = styled.div`
   position: relative;
   cursor: pointer;
   transition: transform 0.2s;
+  
   &:hover {
     transform: scale(1.3);
     z-index: 99;
   }
+  
   @media (min-width: 1200px) {
     &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
       display: block;
       z-index: 100;
     }
   }
+  
   &:first-of-type {
     margin-left: 56px;
+    
     @media (max-width: 1000px) {
       margin-left: 30px;
     }
   }
+
   &:last-of-type {
     margin-right: 56px;
+    
     @media (max-width: 1000px) {
       margin-right: 30px;
     }
@@ -115,6 +124,7 @@ export const FeatureText = styled.p`
   color: white;
   font-weight: ${({ fontWeight }) => (fontWeight === 'bold' ? 'bold' : 'normal')};
   margin: 0;
+
   @media (max-width: 600px) {
     line-height: 22px;
   }
@@ -130,14 +140,17 @@ export const Feature = styled.div`
   background-position-x: right;
   background-repeat: no-repeat;
   background-color: black;
+  
   @media (max-width: 1000px) {
     height: auto;
     background-size: auto;
+    
     ${Title} {
       font-size: 20px;
       line-height: 20px;
       margin-bottom: 10px;
     }
+    
     ${FeatureText} {
       font-size: 14px;
     }
@@ -156,6 +169,7 @@ export const FeatureClose = styled.button`
   cursor: pointer;
   background-color: transparent;
   border: 0;
+  
   img {
     filter: brightness(0) invert(1);
     width: 24px;
@@ -166,6 +180,7 @@ export const Content = styled.div`
   margin: 56px;
   max-width: 500px;
   line-height: normal;
+  
   @media (max-width: 1000px) {
     margin: 30px;
     max-width: none;
