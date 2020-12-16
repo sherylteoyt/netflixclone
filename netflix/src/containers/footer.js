279793ca06e64eaf0f6c9c70c6819a51a39d4a6d
Sheryl Teo <sheryl.teo@ntuclearninghub.com>
2020-12-16 14:17:40 +0800

    added in live links for footer

--- netflix/src/containers/footer.js
+++ netflix/src/containers/footer.js
@@ -8,32 +8,32 @@ export function FooterContainer() {
       <Footer.Break />
       <Footer.Row>
         <Footer.Column>
-          <Footer.Link href="#">FAQ</Footer.Link>
-          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="https://help.netflix.com">FAQ</Footer.Link>
+          <Footer.Link href="http://ir.netflix.com/">Investor Relations</Footer.Link>
           <Footer.Link href="#">Ways to Watch</Footer.Link>
-          <Footer.Link href="#">Corporate Information</Footer.Link>
-          <Footer.Link href="#">Netflix Originals</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/legal/corpinfo">Corporate Information</Footer.Link>
+          <Footer.Link href="https://www.netflix.com/sg/browse/genre/839338">Netflix Originals</Footer.Link>
         </Footer.Column>
 
         <Footer.Column>
-          <Footer.Link href="#">Help Centre</Footer.Link>
-          <Footer.Link href="#">Jobs</Footer.Link>
-          <Footer.Link href="#">Terms of Use</Footer.Link>
-          <Footer.Link href="#">Contact Us</Footer.Link>
+          <Footer.Link href="https://help.netflix.com">Help Centre</Footer.Link>
+          <Footer.Link href="https://jobs.netflix.com/jobs">Jobs</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/legal/termsofuse">Terms of Use</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/contactus">Contact Us</Footer.Link>
         </Footer.Column>
 
         <Footer.Column>
           <Footer.Link href="#">Account</Footer.Link>
           <Footer.Link href="#">Redeem Gift Cards</Footer.Link>
-          <Footer.Link href="#">Privacy</Footer.Link>
-          <Footer.Link href="#">Speed Test</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/legal/privacy">Privacy</Footer.Link>
+          <Footer.Link href="https://fast.com">Speed Test</Footer.Link>
         </Footer.Column>
 
         <Footer.Column>
-          <Footer.Link href="#">Media Centre</Footer.Link>
+          <Footer.Link href="https://media.netflix.com/">Media Centre</Footer.Link>
           <Footer.Link href="#">Buy Relations</Footer.Link>
-          <Footer.Link href="#">Cookie Preferences</Footer.Link>
-          <Footer.Link href="#">Legal Notices</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/legal/privacy#cookies">Cookie Preferences</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/legal/notices">Legal Notices</Footer.Link>
         </Footer.Column>
       </Footer.Row>
       <Footer.Break />
